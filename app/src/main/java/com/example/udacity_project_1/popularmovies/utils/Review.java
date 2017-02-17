82c26039358491097bcf9a9a996f24cc910e6c23
Jorge Santamaría <jsc_@gmx.es>
2017-02-17 19:58:34 +0100

    Added Trailer and Review classes code

--- app/src/main/java/com/example/udacity_project_1/popularmovies/utils/Review.java
+++ app/src/main/java/com/example/udacity_project_1/popularmovies/utils/Review.java
@@ -5,4 +5,21 @@ package com.example.udacity_project_1.popularmovies.utils;
  */
 
 public class Review {
+    private String author;
+    private String content;
+
+    Review(String nAuthor, String nContent) {
+        author = nAuthor;
+        content = nContent;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
 }
+
--- app/src/main/java/com/example/udacity_project_1/popularmovies/utils/Trailer.java
+++ app/src/main/java/com/example/udacity_project_1/popularmovies/utils/Trailer.java
@@ -1,8 +1,31 @@
 package com.example.udacity_project_1.popularmovies.utils;
 
+import java.net.URL;
+
+import static android.provider.ContactsContract.CommonDataKinds.Website.URL;
+
 /**
  * Created by hit on 17/02/17.
  */
 
+
 public class Trailer {
+
+    private String name;
+    private java.net.URL url;
+
+    Trailer(String nName, URL nUrl) {
+        name = nName;
+        url = nUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public URL getUrl() {
+        return url;
+    }
 }
+
+
